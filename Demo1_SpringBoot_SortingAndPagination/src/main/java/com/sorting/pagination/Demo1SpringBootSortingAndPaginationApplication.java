e60a6606dd247ad05e101ff267e2f472d3926cef
Ranju <ranjupillai2016@gmail.com>
2023-07-03 20:19:04 +0530

    First commit for Spring Boot - Sorting and Pagination

---
+++ Demo1_SpringBoot_SortingAndPagination/src/main/java/com/sorting/pagination/Demo1SpringBootSortingAndPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.sorting.pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1SpringBootSortingAndPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1SpringBootSortingAndPaginationApplication.class, args);
+	}
+
+}
---
+++ Demo1_SpringBoot_SortingAndPagination/src/test/java/com/sorting/pagination/Demo1SpringBootSortingAndPaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorting.pagination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1SpringBootSortingAndPaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
